2efc349c27221c60386a00516a8452e9312ab9e5
faizulahasun-cloud <faizul.ahasun@gmail.com>
2026-09-08 13:01:18 +0600

    V2: add Frame019 formula

---
+++ Version2/Frames/Frame019.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// V2 Frame 019
+// 512-voxel frame formula.
+// Coordinates: X=0..7, Y=0..7, Z=0..7.
+// Frame index: F=18.
+
+#define FRAME019_INDEX 18
+
+// Returns true when voxel (X,Y,Z) is ON for this frame.
+inline bool frame019Voxel(uint8_t X, uint8_t Y, uint8_t Z) {
+    const uint8_t F = FRAME019_INDEX;
+    (void)F;
+    (void)X;
+    (void)Y;
+    (void)Z;
+    return false;
+}
